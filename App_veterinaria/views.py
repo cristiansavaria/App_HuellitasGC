cf7e54d4a21db330fd1b348799ed25ff5dbf54c2
cristiansavaria <cristian2012sa@gmail.com>
2022-04-22 21:20:46 -0400

    se agregan los primeros cambios html static

--- App_veterinaria/views.py
+++ App_veterinaria/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
-# Create your views here.
+# Create your views here.se crea primero la vista luego se agrega la urls en e l modulo
+def base(request):
+    return render(request, 'app/base.html')
+def servicio(request):
+    return render(request, 'app/servicios.html')
+def reserva_hora(request):
+    return render(request, 'app/reserva_hora.html')
+def contacto(request):
+    return render(request, 'app/contacto.html')
+def quienes_somos(request):
+    return render(request, 'app/quienes_somos.html')
+
